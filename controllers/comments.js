897df06f8cd7c01f991215fda729cc446f3122fc
uttamthegoat <19516uttam@gmail.com>
2023-08-14 22:10:05 +0530

    Changes in comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,24 +5,18 @@ const asyncErrorHandler = require("../middleware/asyncErrorHandler");
 
 // add a comment  //send the id of the post in req.params
 exports.createComments = asyncErrorHandler(async (req, res) => {
-  const { comment, user, email } = req.body;
+  const { comment, user } = req.body;
   const post = await Post.findById(req.params.id);
   if (!post) throw new CustomError(404, false, "Post not found");
 
   const old_Comments = await Comment.findOne({ post: post.id });
   if (!old_Comments) throw new CustomError(404, false, "Comment not found");
-
-  old_Comments.comments = old_Comments.comments.concat({
-    comment,
-    user,
-    email,
-  });
+  old_Comments.comments = [{ comment, user }].concat(old_Comments.comments);
   const { comments } = await old_Comments.save();
+  if (!comments) throw new CustomError(400, false, "Comment was not added");
   res.status(200).json({
     success: true,
     message: "comment added successfully",
-    totalComments: comments.length,
-    comment: comments,
   });
 });
 
